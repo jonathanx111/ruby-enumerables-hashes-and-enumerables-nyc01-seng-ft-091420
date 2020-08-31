71afc5c4895f966a56adaf911ad63b9a8a553bf3
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-31 03:05:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,3 +12,12 @@ def select_winner(passengers)
       put "#{value} is the lucky winner!"
     end
 end
+
+select_winner(passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
+)
