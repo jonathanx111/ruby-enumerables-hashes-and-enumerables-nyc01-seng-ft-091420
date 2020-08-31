cc9bd61622bf58f7c00af25fe088f4bc3f2ffce6
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-31 03:08:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   passengers.each do |key, value|
-    if key == :suite_a && value[0] = "A"
+    if key == :suite_a && value.start_with("A")
       puts "#{value} is the lucky winner!"
     end
 end
