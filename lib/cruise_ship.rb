8e2fe78d6a99d0291279088de2f4b0bf8c4747f4
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-31 03:12:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,10 +6,3 @@ def select_winner(passengers)
   end
 end
 
-select_winner({
-suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
-suite_e: "Crumpet the Elf"
-})
